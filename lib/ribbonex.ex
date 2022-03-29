46838f36d573296a428ea9fcccf851308cb5f95d
Jon Gautsch <jon@preferral.com>
2022-03-28 20:39:09 -0700

    ⚡️ first commit

---
+++ lib/ribbonex.ex
@@ -0,0 +1,18 @@
+defmodule Ribbonex do
+  @moduledoc """
+  Documentation for `Ribbonex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ribbonex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ribbonex_test.exs
@@ -0,0 +1,8 @@
+defmodule RibbonexTest do
+  use ExUnit.Case
+  doctest Ribbonex
+
+  test "greets the world" do
+    assert Ribbonex.hello() == :world
+  end
+end
